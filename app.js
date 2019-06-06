3487b42426f44e576eaa4e1010c3cb5ce2b3e705
IdeaSailorTechnologies <you@example.com>
2019-06-06 16:57:44 +0530

    headers file changes

--- app.js
+++ app.js
@@ -7,42 +7,42 @@ const mongoose = require('mongoose');
 //const productRoutes = require('./api/routes/products');
 const customerRoutes = require('./routes/customerRoutes');
 
-mongoose.connect('mongodb://localhost:27017/CTSDB', {useNewUrlParser: true})
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/CTSDB', {useNewUrlParser: true});
+mongoose.set('useCreateIndex', true);
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use((req, res, next) =>{
-    res.header("Access-Control-Allow-Origin","*");
-    res.header(
-        "Acess-Control-Allow-Headers",
-        "Origin, X-Requested With, Content-Type, Accept, Authorization"
-        );
-        if(req.method === 'OPTIONS'){
-            res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET')
-            return res.status(200).json({});
-        }
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin','*'); // in future allow only for leoroc apps, by putting urls. video- 5 parsing the body @11:00
+    res.header('Access-Control-Allow-Headers','*');// Origin,X-Requested-With,Content-Type,Accept,Authorization
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,PATCH');
+        res.status(200).json({});
+    }
+    next();
 });
 
-// routes should handle request
-//app.use('/products', productRoutes);
+
 app.use('/customers', customerRoutes);
 
 
-app.use(( req, res,next) =>{
-    const error = new error ("Not Found")
-    next (error);
+app.use((req,res,next) => {
+    const error = new Error('page not found');
+    error.status = 404;
+    next(error);
 });
 
-app.use((error, req, res,next) =>{
+app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
-        error:{
-            message: error.message
+        error : {
+            message : error.message
         }
-    })
-
+    });
 });
 
 module.exports = app;

8273f1413bd980824c4f2a41df31e09251f84e32
IdeaSailorTechnologies <you@example.com>
2019-06-07 17:01:29 +0530

    Login api for customer

--- controller/customerController.js
+++ controller/customerController.js
@@ -3,6 +3,9 @@ const customercontroller = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const Customer = require('../models/customerModel');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
 const { check, validationResult } = require('express-validator/check');
 
 
@@ -62,3 +65,57 @@ exports.saveCustomer = (req, res, next) => {
         });
     }) 
 };
+
+exports.loginCustomer = (req, res, next) =>{
+    var token;
+    Customer.find({ emailAddress: req.body.emailAddress})
+    .exec()
+    .then(result =>{
+        if(result.length != 1){
+            res.status(409).json({
+                errorMessage:"Auth failed, please check your email"
+            })
+        }
+        bcrypt.compare(req.body.password, result[0].password, (err, success) =>{
+            if(!success){
+                res.status(409).json({
+                    errorMessage:"Auth failed, please check your password"
+                })
+            }
+            if(success){
+                token = jwt.sign(
+                    {
+                        emailAddress : result[0].emailAddress,
+                        userId: result[0]._id
+                    }, 
+                    config.env.JWT_KEY,
+                    {
+                        expiresIn: "10h"
+                    }
+                )
+                Customer.update({_id: result[0]._id}, {fcmToken: req.body.fcmToken})
+                .then(doc=>{
+                    res.status(201).json({
+                        message:"Login success",
+                        _id: result[0]._id,
+                        companyName: result[0].companyName,
+                        emailAddress: result[0].emailAddress,
+                        mobile: result[0].mobile,
+                        fcmToken: req.body.fcmToken,
+                        jwt: token
+                    })
+                }).catch(err =>{
+                    res.status(500).json({
+                        error:err
+                    })
+                })
+            }
+        })
+    })
+    .catch(err =>{
+        res.status(500).json({
+            errorMessage:"Auth failed, please check your email & password"
+        })
+    })
+
+}
